6da0ac71ab39eb5ecff797dd1561274552f6a416
rr210 <1577791638@qq.com>
2021-12-26 16:23:02 +0800

    update .gitignore file

--- vue.config.js
+++ vue.config.js
@@ -4,14 +4,14 @@
  * @Date: 2021-11-11 10:13:01
  * @Url: https://u.mr90.top
  * @github: https://github.com/rr210
- * @LastEditTime: 2021-11-11 10:13:01
+ * @LastEditTime: 2021-12-26 16:21:55
  * @LastEditors: Harry
  */
 // vue.config.js
 const path = require('path')
 module.exports = {
   publicPath: './', // 文件加载设置为相对路径
-  outputDir: 'dist',
+  outputDir: `dist-${process.env.NODE_ENV}`,
   // lintOnSave: false, // 关闭eslint
   // productionSourceMap: true, // 生产环境下css 分离文件
   configureWebpack: {
